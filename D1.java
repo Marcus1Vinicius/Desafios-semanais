b891eff4b25bb21ae96884c6bf594ff0819670a0
Marcus <omarcusvinicius96@gmail.com>
2026-03-05 18:36:29 -0300

    desafio D1

---
+++ D1.java
@@ -0,0 +1,15 @@
+//desafio da sequencia de Fibonacci, a ideia e fazer um algoritmo que solicita ao usuario um numero e o algoritmo vai mostrar a sequencia de Fibonacci
+// ate o numero que o usuario digitou, a sequencia se da pelo numero sucessor ser a soma dos dois anteriores a ele: 0 1 1 2 3 5...
+import java.util.Scanner;
+public class D1 {
+
+    public static void main(int[] args){
+
+        Scanner leitor = new Scanner(System.in);
+        int numero;
+
+        System.out.print("Digite o numero: ");
+        numero = leitor.nextInt();
+    }
+
+}
