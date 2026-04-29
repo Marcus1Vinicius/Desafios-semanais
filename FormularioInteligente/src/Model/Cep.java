6f9753ae1a4e5458241b7b7386b080589e261605
Marcus <omarcusvinicius96@gmail.com>
2026-04-29 19:36:20 -0300

    Classes cep e password finalizadas

--- FormularioInteligente/src/Model/Cep.java
+++ FormularioInteligente/src/Model/Cep.java
@@ -1,5 +1,35 @@
 package Model;
 
 public class Cep {
-    String cep;
+    String cepDigits;
+
+    protected Cep(){}
+
+    public String validateCep(String cepInput){
+
+        if(!formatCheck(cepInput)){
+            throw new IllegalArgumentException("CEP errado!");
+        }
+
+        return cepDigits;
+    }
+
+    public boolean formatCheck(String cepDigits){
+
+        if(cepDigits == null){
+            throw new IllegalArgumentException("CEP nulo!");
+        }
+
+        String cepClean = cepDigits.replaceAll("[^a-zA-Z0-9\\s]", "");
+
+        if(!cepDigits.matches("^\\\\d+$")){
+            throw new IllegalArgumentException("O cep deve conter apenas números!");
+        }
+
+        if(!cepClean.matches("\\d{8}")){
+            throw new IllegalArgumentException("CEP deve conter exatamente 8 números.");
+        };
+
+        return cepDigits.matches("^\\d{5}\\d{8}$");
+    }
 }
--- FormularioInteligente/src/Model/Password.java
+++ FormularioInteligente/src/Model/Password.java
@@ -1,5 +1,31 @@
 package Model;
 
 public class Password {
-    String passarword;
+    String password;
+
+    public String checkPassword(String passwordInput){
+
+        if(!validatePassword(passwordInput)){
+            throw new IllegalArgumentException("Senha inválida! A senha precisa de 8 caracteres(letras maiusculas e minuscula, digitos numericos e caracteres especiais '@#$%' ");
+        }
+
+        this.password = passwordInput;
+
+        return password;
+    }
+
+    public boolean validatePassword(String password){
+
+        if(password == null){
+            throw new IllegalArgumentException("The password cannot null!");
+        };
+
+        if(password.length() < 8){
+            throw new IllegalArgumentException("A senha precisar ter no minimo 8 caracteres");
+        };
+
+        return password.matches("^(?=.*[A-Z])(?=.*\\d)(?=.*[@#$%^&+=!]).+$");
+
+    }
+
 }
